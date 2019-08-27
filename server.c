08ec9bac9c21c587be5b64eaf59377519ce9af60
amrendra singh rathore <31726307+daredevil786@users.noreply.github.com>
2019-08-27 15:13:40 +0530

    Add files via upload

---
+++ server.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<arpa/inet.h>
+#include<fcntl.h>
+#include<unistd.h>
+int main()
+{
+int welcome,new_soc,fd,n;
+char buffer[1024],fname[50];
+struct sockaddr_in addr;
+welcome=socket(PF_INET,SOCK_STREAM,0);
+addr.sin_family=AF_INET;
+addr.sin_port=htons(7891);
+addr.sin_addr.s_addr=inet_addr("127.0.0.1");
+bind(welcome,(struct sockaddr*)&addr,sizeof(addr));
+printf("\nServer is online");
+listen(welcome,5);
+new_soc=accept(welcome,NULL,NULL);
+recv(new_soc,fname,50,0);
+printf("\nRequesting for file:%s\n",fname);
+fd=open(fname,O_RDONLY);
+if(fd<0)
+send(new_soc,"\nFile not found\n",15,0);
+else
+while((n=read(fd,buffer,sizeof(buffer)))>0)
+send(new_soc,buffer,n,0);
+printf("\nRequest sent\n");
+close(fd);
+return 0;
+}
+
